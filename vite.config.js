e018319567022e5673fe2df8c4185cc4869a53ad
RaccoonDev420 <luisfernando_salmeron@yahoo.com>
2022-03-21 15:26:41 -0600

    new update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 
+    base: '/myCv/',
     build: {
         chunkSizeWarningLimit: 2000,
         assetsInlineLimit: '2048',
